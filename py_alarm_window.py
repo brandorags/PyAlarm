132489243bd44ebb660a520cf7566db28d76f25d
Brandon Ragsdale <brandon@brandonragsdale.com>
2015-03-07 11:32:46 -0500

    finished Tkinter tutorial, NOW IT'S TIME TO WORK

--- py_alarm_window.py
+++ py_alarm_window.py
@@ -5,7 +5,6 @@ Description: This file generates the main window that
 will house the main logic of PyAlarm.
 """
 
-
 import Tkinter
 
 
@@ -18,9 +17,11 @@ class PyAlarmWindow(Tkinter.Tk):
     def initialize_window(self):
         self.grid()
 
-        self.entry = Tkinter.Entry(self)
+        self.entry_variable = Tkinter.StringVar()
+        self.entry = Tkinter.Entry(self, textvariable=self.entry_variable)
         self.entry.grid(column=0, row=0, sticky="EW")
         self.entry.bind("<Return>", self.on_press_enter)
+        self.entry_variable.set(u"Enter text here...")
 
         button = Tkinter.Button(self, text="Find song",
                                 command=self.on_button_click)
@@ -29,15 +30,18 @@ class PyAlarmWindow(Tkinter.Tk):
         self.label_variable = Tkinter.StringVar()
         label = Tkinter.Label(self, textvariable=self.label_variable, anchor="w", fg="white", bg="blue")
         label.grid(column=0, row=2, columnspan=2, sticky="EW")
+        self.label_variable.set(u"Hello!")
 
         self.grid_columnconfigure(0, weight=1)
         self.resizable(True, False)
+        self.update()
+        self.geometry(self.geometry())
 
     def on_button_click(self):
-        self.label_variable.set("You clicked the button!")
+        self.label_variable.set(self.entry_variable.get() + " (You clicked the button)")
 
     def on_press_enter(self, event):
-        print("You pressed enter!")
+        self.label_variable.set(self.entry_variable.get() + " (You pressed ENTER)")
 
 if __name__ == "__main__":
     py_alarm_app = PyAlarmWindow(None)
