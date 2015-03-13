fe70901e15a292f67a89f939daecfec57069fcb0
brandorags <brandorags@users.noreply.github.com>
2015-03-13 02:36:05 -0500

    working on terminating separate process that plays song

--- py_alarm.py
+++ py_alarm.py
@@ -1,5 +1,6 @@
 from datetime import datetime
-from subprocess import call
+from subprocess import Popen, call
+import multiprocessing
 import os
 import random
 import glob
@@ -18,6 +19,7 @@ class PyAlarm(object):
     album_list = None
     random_song = None
     specific_song = None
+    play_specific_song_process = multiprocessing.Process()
 
     def create_and_randomize_song_list(self, user_selected_directory):
         if user_selected_directory:
@@ -30,6 +32,7 @@ class PyAlarm(object):
             # set class variables
             self.album_list = songs_from_user_selected_directory
             self.random_song = random.randint(0, len(self.album_list) - 1)
+            self.play_specific_song_process = multiprocessing.Process(target=self.play_song)
             self.specific_song = None
 
             # return string of random album name for label on PyAlarm window
@@ -37,11 +40,19 @@ class PyAlarm(object):
 
     def store_specific_song(self, user_selected_song):
         self.specific_song = user_selected_song
+        self.play_specific_song_process = multiprocessing.Process(target=self.play_song)
         self.album_list = None
         self.random_song = None
+
+    def create_separate_process_to_play_song(self):
+        self.play_specific_song_process.start()
 
     def play_song(self):
         if self.album_list and self.random_song:
             call(["afplay", self.album_list[self.random_song]])
         elif self.specific_song:
             call(["afplay", str(self.specific_song[0])])
+
+    def stop_song(self):
+        self.play_specific_song_process.terminate()
+        self.play_specific_song_process.join()
