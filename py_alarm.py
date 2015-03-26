e55e42d473715f322c685040e1ff7767784668fb
Brandon Ragsdale <brandon@brandonragsdale.com>
2015-03-25 23:28:59 -0500

    added GPL headers to py_alarm.py and py_alarm_window.py

--- py_alarm.py
+++ py_alarm.py
@@ -1,3 +1,22 @@
+"""
+PyAlarm: An alarm clock written in Python
+Copyright (C) 2015 Brandon Ragsdale
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+"""
+
 from datetime import datetime
 from subprocess import Popen, call
 import os
@@ -30,7 +49,7 @@ class PyAlarm(object):
                           user_selected_directory + '/*.flac',
                           user_selected_directory + '/*.ogg',
                           user_selected_directory + '/*.wav',)
-            
+
             songs_from_user_selected_directory = []
 
             for songs in song_types:
