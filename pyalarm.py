3265aee3cdc58b3c31f7dc913b5884d2f334b3d0
Brandon Ragsdale <brandon@brandonragsdale.com>
2016-08-14 23:25:08 -0600

    added license to file

--- pyalarm.py
+++ pyalarm.py
@@ -1,9 +1,20 @@
 """
-Name: pyalarm.py (Mac version)
-Author: Brandon Ragsdale
-Date: 7/5/2015
-Description: This program is an alarm clock that will play a random
-song from a user's iTunes library and increment the volume at a set rate
+PyAlarm: An alarm clock written in Python
+Copyright (C) 2016 Brandon Ragsdale
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 """
 
 
